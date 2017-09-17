c71b067cbff6298b7c88f41c12a50aa8734bef7a
Alan Tan <alantan02@hotmail.com>
2017-09-17 14:30:42 +0800

    MVCAU-1 Setup media recorder, capturing and saving videos

--- app/src/main/java/com/example/tlaichia/mobilevideocaptureanduploader/MainActivity.java
+++ app/src/main/java/com/example/tlaichia/mobilevideocaptureanduploader/MainActivity.java
@@ -10,6 +10,7 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.media.MediaRecorder;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity {
     private CameraDevice mCameraDevice;
     private String mCameraId;
     private Size mPreviewSize;
+    private Size mVideoSize;
+    private MediaRecorder mMediaRecorder;
     private CaptureRequest.Builder mCaptureRequestBuilder;
     private HandlerThread mBackgroundHandlerThread;
     private Handler mBackgroundHandler;
@@ -73,7 +76,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onOpened(CameraDevice camera) {
             mCameraDevice = camera;
-            startPreview();
+            if(mIsRecording) {
+                try {
+                    createVideoFileName();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                startRecord();
+                mMediaRecorder.start();
+            } else {
+                startPreview();
+            }
         }
 
         @Override
@@ -96,6 +109,8 @@ public class MainActivity extends AppCompatActivity {
 
         createVideoFolder();
 
+        mMediaRecorder = new MediaRecorder();
+
         mTextureView = (TextureView) findViewById(R.id.textureView);
 
         mIsRecording = false;
@@ -106,6 +121,9 @@ public class MainActivity extends AppCompatActivity {
                 if(mIsRecording) {
                     mIsRecording = false;
                     mRecordVideoImageButton.setImageResource(R.mipmap.btn_video_record);
+                    mMediaRecorder.stop();
+                    mMediaRecorder.reset();
+                    startPreview();
                 } else {
                     checkWriteStoragePermission();
                 }
@@ -184,7 +202,8 @@ public class MainActivity extends AppCompatActivity {
                 if(cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT) {
                     continue;
                 }
-                mPreviewSize = new Size(720, 1280);
+                mPreviewSize = new Size(1280, 720);
+                mVideoSize = new Size(1280, 720);
                 mCameraId = cameraId;
                 return;
             }
@@ -209,6 +228,39 @@ public class MainActivity extends AppCompatActivity {
                 cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, mBackgroundHandler);
             }
         } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startRecord() {
+        try {
+            setupMediaRecorder();
+
+            SurfaceTexture surfaceTexture = mTextureView.getSurfaceTexture();
+            surfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+            Surface previewSurface = new Surface(surfaceTexture);
+
+            Surface recordSurface = mMediaRecorder.getSurface();
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_RECORD);
+            mCaptureRequestBuilder.addTarget(previewSurface);
+            mCaptureRequestBuilder.addTarget(recordSurface);
+
+            mCameraDevice.createCaptureSession(Arrays.asList(previewSurface, recordSurface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+                    try {
+                        session.setRepeatingRequest(mCaptureRequestBuilder.build(), null, null);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+
+                }
+            }, null);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
@@ -292,6 +344,8 @@ public class MainActivity extends AppCompatActivity {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
+                startRecord();
+                mMediaRecorder.start();
             } else {
                 if(shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                     Toast.makeText(this, "App needs to be able to save videos", Toast.LENGTH_SHORT).show();
@@ -306,6 +360,20 @@ public class MainActivity extends AppCompatActivity {
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            startRecord();
+            mMediaRecorder.start();
         }
+    }
+
+    private void setupMediaRecorder() throws IOException {
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mMediaRecorder.setOutputFile(mVideoFileName);
+        mMediaRecorder.setVideoEncodingBitRate(5000000);
+        mMediaRecorder.setVideoFrameRate(30);
+        mMediaRecorder.setVideoSize(mVideoSize.getWidth(), mVideoSize.getHeight());
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        mMediaRecorder.setOrientationHint(90);
+        mMediaRecorder.prepare();
     }
 }
